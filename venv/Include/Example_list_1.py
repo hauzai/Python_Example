acc45fb6f73010313acf7a7f7252aaa1dd852302
yaoyihua <yaoyihua@togest.com>
2018-08-13 19:28:57 +0800

    Default Changelist

--- venv/Include/Example_list_1.py
+++ venv/Include/Example_list_1.py
@@ -5,3 +5,13 @@ car_details = ["toyota","rav4",2.2,60807]
 everythings = [temps,price,words,car_details]
 #列表中可以存放 任何数据类型 长度可变
 print("word" not in words)
+
+a = ["a","b","c","d","e","p","b"]
+A = "apple"
+seem = []
+for ch in A:
+    if ch in a:
+        if ch not in seem:
+            seem.append(ch)
+
+print(seem)
