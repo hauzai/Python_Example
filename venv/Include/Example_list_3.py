fbfd31d525789cc3ae9bc6d57941ebbc8959e714
yaoyihua <yaoyihua@togest.com>
2018-08-20 19:55:55 +0800

    列表的练习p67

---
+++ venv/Include/Example_list_3.py
@@ -0,0 +1,12 @@
+#使用list的方法将plist转换为on tap
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+
+found = []
+
+
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
