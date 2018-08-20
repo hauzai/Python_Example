277b4a8a71f365ce5167f3bc8cf407ab9529897f
yaoyihua <yaoyihua@togest.com>
2018-08-20 19:20:47 +0800

    20180816

--- venv/Include/Example_list_1.py
+++ venv/Include/Example_list_1.py
@@ -6,14 +6,14 @@ everythings = [temps,price,words,car_details]
 #列表中可以存放 任何数据类型 长度可变
 print("word" not in words)
 
-a = ["a","b","c","d","e","p","b"]
-A = "apple"
+a = ["a","e","i","o","u"]
+A = input("请输入一个单词，查看单词中的元音:")
 seem = []
 for ch in A:
     if ch in a:
         if ch not in seem:
             seem.append(ch)
-
-print(seem)
-print(seem)
+#append 函数，为列表中添加对象
+for ch in seem:
+    print(ch)
 print("hellogongsi")
