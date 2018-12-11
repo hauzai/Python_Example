1c79dd85232a655de0cb629d29759bd24b935d5c
yaoyihua <yaoyihua@togest.com>
2018-12-11 11:33:15 +0800

    注释后添加空行

--- venv/Include/Example_list_1.py
+++ venv/Include/Example_list_1.py
@@ -3,7 +3,7 @@ temps = [110.0,20.5]
 words = ["hello","word"]
 car_details = ["toyota","rav4",2.2,60807]
 everythings = [temps,price,words,car_details]
-#列表中可以存放 任何数据类型 长度可变
+# 列表中可以存放 任何数据类型 长度可变
 print("word" not in words)
 
 a = ["a","e","i","o","u"]
@@ -13,7 +13,7 @@ for ch in A:
     if ch in a:
         if ch not in seem:
             seem.append(ch)
-#append 函数，为列表中添加对象！
+# append 函数，为列表中添加对象！
 for ch in seem:
     print(ch)
 print("hellogongsi")
